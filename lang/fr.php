2a7f9a1e5ef8ecbab383fb4815a762eccca31e64
Cyril MAGUIRE <github@ecyseo.net>
2015-03-12 15:32:11 +0100

    First commit

---
+++ lang/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+$LANG = array(
+
+'L_PAGE_TITLE'				=> 'Déconnexion',
+
+# config.php
+'L_MENU_DISPLAY'			=> 'Afficher le menu de la page de contact',
+'L_MENU_TITLE'		    	=> 'Titre du menu',
+'L_MENU_POS'		    	=> 'Position du menu',
+'L_TIMEOUT'		    		=> 'Temps de déconnexion en minutes (laissez à 0 pour un temps de connexion infini)',
+'L_SAVE'					=> 'Enregistrer',
+
+'L_DEFAULT_MENU_NAME'		=> 'Déconnexion',
+
+);
+?>
