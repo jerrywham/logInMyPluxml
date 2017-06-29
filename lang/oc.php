6f287323caa59eb77ba6f77cbc0603467776abae
Quent-in <pages_quentin@hotmail.com>
2017-06-29 13:31:50 +0200

    Création de oc.php

---
+++ lang/oc.php
@@ -0,0 +1,17 @@
+<?php
+
+$LANG = array(
+
+'L_PAGE_TITLE'				=> 'Desconnexion',
+
+# config.php
+'L_MENU_DISPLAY'			=> 'Afichar lo menú de la pagina de contacte',
+'L_MENU_TITLE'		    => 'Títol del menú',
+'L_MENU_POS'		    	=> 'Posicion del menú',
+'L_TIMEOUT'		    		=> 'Temps de desconnexion en minutas (Daissatz a 0 per un temps de connexion infinida)',
+'L_SAVE'					    => 'Enregistrar',
+
+'L_DEFAULT_MENU_NAME'	=> 'Desconnexion',
+
+);
+?>
